f9a7202434149f0746d5ea34a665191efd29df6e
Rachel Degany <racheldegany@gmail.com>
2020-12-14 11:48:10 +0200

    http

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -1,9 +1,12 @@
+
 module.exports = {
     cookieName: 'ins_user',
     secret: 'kdj80dnjd9',
     dbUrl: 'mongodb://localhost:27017/instagram',
     port: 4000,
-    dev: true
+    dev: true,
+    // key: fs.readFileSync('server.key'),
+    // cert: fs.readFileSync('server.cert')
     
 }
 
--- src/server.js
+++ src/server.js
@@ -4,9 +4,22 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./config/routes');
+var https = require('https');
+const fs = require('fs');
+// var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+// var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 const app = express();
 const config = require('./config/env/index');
 const port = config.port;
+var credentials = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+};
+// const httpsServer = https.createServer(credentials, app);
+
+
+
+
 
 app.use(cors({
     origin: true,
@@ -21,6 +34,8 @@ app.use(routes);
 connect();
 
 function listen() {
+    // console.log(credentials);
+    // https.createServer(credentials, app).listen(port, () => console.log(`server is listening on port ${port}`));
     app.listen(port, () => console.log(`server is listening on port ${port}`));
 }
 
